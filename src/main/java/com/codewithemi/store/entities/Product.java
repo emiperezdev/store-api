6e8dbdc3311d2db09b9d67e330f814ccb7e45b1f
emiperezdev <emiperez.dev@gmail.com>
2025-07-14 19:48:29 -0600

    add: migrations using model first with jpa buddy

--- src/main/java/com/codewithemi/store/entities/Product.java
+++ src/main/java/com/codewithemi/store/entities/Product.java
@@ -21,6 +21,9 @@ public class Product {
   @Column(name = "name")
   private String name;
 
+  @Column(name = "description")
+  private String description;
+
   @Column(name = "price")
   private Double price;
 
