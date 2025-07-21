d2523f75362b12c06787e6cad0f5d9f37f3d1311
emiperezdev <emiperez.dev@gmail.com>
2025-07-20 19:20:11 -0600

    add: using repositories

--- src/main/java/com/codewithemi/store/StoreApplication.java
+++ src/main/java/com/codewithemi/store/StoreApplication.java
@@ -1,8 +1,5 @@
 package com.codewithemi.store;
 
-import com.codewithemi.store.entities.Profile;
-import com.codewithemi.store.entities.User;
-import com.codewithemi.store.repositories.ProfileRepository;
 import com.codewithemi.store.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,28 +10,9 @@ public class StoreApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-//		var profileRepository = context.getBean(ProfileRepository.class);
-		var userRepository = context.getBean(UserRepository.class);
-		var user = User.builder()
-			.name("John")
-			.email("email")
-			.password("password")
-			.build();
+		var repository = context.getBean(UserRepository.class);
 
-		userRepository.save(user);
-		System.out.println(user);
-//
-//		var profile = Profile.builder()
-//			.bio("My bio")
-//			.phoneNumber("123456789")
-//			.loyaltyPoints(1000)
-//			.build();
-//
-//		var user = userRepository.findById(1L).get();
-//		user.addProfile(profile);
-//		profileRepository.save(profile);
-//
-//		System.out.println(user);
+		repository.deleteById(1L);
 	}
 
 }
